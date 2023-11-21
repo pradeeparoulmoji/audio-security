81846b75d21a44400834dfc2a1d90fe2696bdf42
PRADEEP AROULMOJI <pradaroulmojiai@gmail.com>
2023-11-21 10:49:49 +0100

    small change

--- scripts/noise_reduction_clean.py
+++ scripts/noise_reduction_clean.py
@@ -116,18 +116,3 @@ class PreProcessing:
   
 
 
-file_path = "C:/Users/tm2378/Desktop/TIROCINIO/WAV/more2.wav"
-file_path_noise = "C:/Users/tm2378/Desktop/TIROCINIO/WAV/noises.wav"
-file_path_dest = 'C:/Users/tm2378/Desktop/TIROCINIO/WAV/effectedoooooooooooooo.wav'
-processor = PreProcessing(file_path, file_path_dest)
-processor.read_audio(file_path,file_path_noise)
-
-processor.process_audio()
-#processor.plot_audio_channels() #original audio plot
-
-#processor.read_audio(file_path_dest)
-#processor.plot_audio_channels() #processed audio plot
-
-print("finished")
-
-#Note finali: ad ogni finestra matplotlib che si apre, per visualizzare la successiva bidsogna chiudere la precedente
