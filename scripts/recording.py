d407aaee6432649578deaa9743ae41f72738e3f6
PRADEEP AROULMOJI <pradaroulmojiai@gmail.com>
2023-12-02 19:43:22 +0100

    Update recording.py

--- scripts/recording.py
+++ scripts/recording.py
@@ -14,26 +14,16 @@ class Recording:
 
     # Record audio
     def record(self):
-        for i in range(int(self.duration/self.split)): # number of chunks
-         self.recording.append(sd.rec(int(self.split * self.fs), samplerate=self.fs, channels=2))
+       
+     for i in range(int(self.duration/self.split)): # number of chunks
+        output_file = f"stereo_audio3{i}.wav"    # for each itearation, create a new file name
+        self.recording.append(sd.rec(int(self.split * self.fs), samplerate=self.fs, channels=2)) # record i-th chunk
+        sd.wait()
+        scaled_audio_data = np.int16(self.recording[i] * 32767) # Scale the audio data to the appropriate range for 16-bit PCM (typically -32768 to 32767)
+        write(output_file, self.fs, scaled_audio_data) # Save the chunk data as a WAV file
         
-        # Wait for the recording to finish
-         sd.wait()
-        
-        return self.recording
-
-    def write(self):
-        for i in range(len(self.recording)):
-            
-            output_file = f"stereo_audio{i}.wav" 
-            # Scale the audio data to the appropriate range for 16-bit PCM (typically -32768 to 32767)
-            scaled_audio_data = np.int16(self.recording[i] * 32767)
-            # Save the audio data as a WAV file
-            write(output_file, self.fs, scaled_audio_data)
-
     
-a = Recording()
-print(len(a.record()))
-a.write()
+
+
 
 
